50ed3858adec46887fdce01daa1529bb256d7e45
David Krcek <david.krcek@consolut.com>
2024-07-26 15:24:57 +0200

    commit Lightbox and SafeAre

---
+++ MediumDemoApps/Lightbox.swift
@@ -0,0 +1,18 @@
+//
+//  Lightbox.swift
+//  MediumDemoApps
+//
+//  Created by David Krcek on 26.07.24.
+//
+
+import SwiftUI
+
+struct Lightbox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lightbox()
+}
---
+++ MediumDemoApps/SafeAreaView.swift
@@ -0,0 +1,18 @@
+//
+//  SafeAreaView.swift
+//  MediumDemoApps
+//
+//  Created by David Krcek on 26.07.24.
+//
+
+import SwiftUI
+
+struct SafeAreaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafeAreaView()
+}
