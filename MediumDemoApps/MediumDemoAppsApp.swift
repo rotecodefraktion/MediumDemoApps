e67e356ecb2f9c13b777fda5c97a5086b8dc23b7
David Krcek <david.krcek@consolut.com>
2024-07-26 15:19:29 +0200

    Initial Commit

---
+++ MediumDemoApps/MediumDemoAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumDemoAppsApp.swift
+//  MediumDemoApps
+//
+//  Created by David Krcek on 26.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MediumDemoAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediumDemoAppsTests/MediumDemoAppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediumDemoAppsTests.swift
+//  MediumDemoAppsTests
+//
+//  Created by David Krcek on 26.07.24.
+//
+
+import Testing
+@testable import MediumDemoApps
+
+struct MediumDemoAppsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
