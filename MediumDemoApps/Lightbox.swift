6da3533e52ef291de1cd5efa1dc9736c18eb9168
David Krcek <david.krcek@consolut.com>
2024-07-26 15:25:14 +0200

    commit Lightbox and SafeAre

--- MediumDemoApps/Lightbox.swift
+++ MediumDemoApps/Lightbox.swift
@@ -2,14 +2,155 @@
 //  Lightbox.swift
 //  MediumDemoApps
 //
-//  Created by David Krcek on 26.07.24.
+//  Created by David Krcek on 18.07.24.
 //
+
 
 import SwiftUI
 
 struct Lightbox: View {
+    
+    let rows: [GridItem] = [
+        GridItem(.flexible(), spacing: 0, alignment: nil ),
+        GridItem(.flexible(), spacing: 0, alignment: nil ),
+    ]
+    
+    let imageUrl: URL = URL(string: "https://picsum.photos/200")!
+    @State private var mainImage: Image?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            NavigationStack {
+                VStack(spacing: 15) {
+                    if let image = mainImage {
+                        image
+                            .resizable()
+                            .frame(maxWidth: .infinity)
+                            .frame(height: UIScreen.main.bounds.height * 0.40)
+                            .scaledToFill()
+                            .clipShape(.rect(cornerRadius: 10))
+                        
+                    } else {
+                        AsyncImage(url: imageUrl, content: { returnImage in
+                            returnImage
+                                .resizable()
+                                .frame(maxWidth: .infinity)
+                                .frame(height: UIScreen.main.bounds.height * 0.40)
+                                .scaledToFill()
+                                .clipShape(.rect(cornerRadius: 10))
+                        }) {
+                            ProgressView()
+                        }
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(
+                            rows: rows,
+                            alignment: .center,
+                            spacing: 10,
+                            pinnedViews: .sectionHeaders) {
+                                Section(header:
+                                            Text("1st Section")
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.pink)
+                                    .frame(height: 40)
+                                    .frame(width: 300)
+                                    .background(
+                                        Color(UIColor(.primary.opacity(0.05)))
+                                            .overlay(.ultraThinMaterial)
+                                        
+                                    )
+                                        .rotationEffect(Angle(degrees: 270))
+                                        .padding(.trailing, -125)
+                                        .padding(.leading, -130)
+                                        
+                                )
+                                {
+                                    ForEach(0..<10) { index in
+                                        AsyncImage(url: imageUrl) { phase in
+                                            switch phase {
+                                            case .empty:
+                                                ProgressView()
+                                            case .success(let returnImage):
+                                                returnImage
+                                                    .resizable()
+                                                    .frame(height: 145)
+                                                    .frame(width: 145)
+                                                    .scaledToFill()
+                                                    .clipShape(.rect(cornerRadius: 10))
+                                                    .onTapGesture {
+                                                        mainImage = returnImage
+                                                    }
+                                            default:
+                                                ProgressView()
+                                            }
+                                            
+                                        }
+                                        
+                                        
+                                    }
+                                    
+                                }
+                                
+                                Section(header:
+                                            Text("2nd Section")
+                                    .font(.subheadline)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.pink)
+                                    .frame(height: 40)
+                                    .frame(width: 300)
+                                    .background(
+                                        Color(UIColor(.primary.opacity(0.05)))
+                                            .overlay(.ultraThinMaterial)
+                                        
+                                    )
+                                        .rotationEffect(Angle(degrees: 270))
+                                        .padding(.trailing, -130)
+                                        .padding(.leading, -130)
+                                        
+                                ){
+                                    ForEach(0..<10) { index in
+                                        AsyncImage(url: imageUrl) { phase in
+                                            switch phase {
+                                            case .empty:
+                                                ProgressView()
+                                            case .success(let returnImage):
+                                                returnImage
+                                                    .resizable()
+                                                    .frame(height: 145)
+                                                    .frame(width: 145)
+                                                    .scaledToFill()
+                                                    .clipShape(.rect(cornerRadius: 10))
+                                                    .shadow(radius: 5)
+                                                    .onTapGesture {
+                                                        mainImage = returnImage
+                                                    }
+                                            default:
+                                                ProgressView()
+                                            }
+                                            
+                                        }
+                                        
+                                        
+                                    }
+                                }
+                                
+                                
+                            }
+                        
+                        
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal, 14)
+                .padding(.vertical, 3)
+                .toolbar(.hidden, for: .navigationBar)
+                .safeAreaInset(edge: .top) {
+                    SafeAreaView(screenTitle: "LightBox",
+                                 screenSubtitle: "It's time to browse through your beautiful pictures!")
+                }
+            }
+        }
     }
 }
 
--- MediumDemoApps/SafeAreaView.swift
+++ MediumDemoApps/SafeAreaView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct SafeAreaView: View {
+    
+    /// SafeArea NavigationBar
+    /// Optional parameter:
+    /// showBackbutton: Bool  to toogle the dispay of an left backbutton, default true
+    /// showRightButton: Bool  to toogle the dispay of an right button, default: false
+    /// rightButtonIcon: String for systemName Icon, default ""
+    /// Mandantory parameter:
+    /// screenTitle: Title of navigation bar, mandantory paramter
+    /// screenSubtitle: Subtitle of navigation bar
+    ///
+    var showBackButton: Bool = false
+    var showRightButton: Bool = false
+    var rightButtonIcon: String = ""
+    let screenTitle: String
+    let screenSubtitle: String
+    
+    
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 6) {
+            HStack() {
+                if showBackButton {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.backward")
+                                .font(.title2)
+                                    .foregroundColor(.black)
+                            Text("Back")
+                                .font(.headline)
+                                    .foregroundColor(.black)
+                        }
+                        
+                    }
+                }
+                Spacer()
+                Text(screenTitle)
+                    .font(.title.weight(.bold))
+                Spacer()
+                if showRightButton {
+                    Button(action: {}) {
+                        Image(systemName: rightButtonIcon)
+                            .font(.title)
+                            .foregroundColor(.pink)
+                    }
+                }
+            }
+            Text(screenSubtitle)
+            //Text("Here is a small List of Apple Silicon devices")
+                .font(.caption)
+        }
+        .padding()
+        .background(LinearGradient(colors: [.purple.opacity(0.3), .pink.opacity(0.7)],
+                                   startPoint: .topLeading, endPoint: .bottomTrailing)
+            .overlay(.ultraThinMaterial)
+        )
     }
 }
 
+
 #Preview {
-    SafeAreaView()
+    SafeAreaView(screenTitle: "Title", screenSubtitle: "This is a subtitle")
 }
